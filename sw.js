4db9e651b1a12c459d15ccd4f90eb0b1aaad04d0
Felipe Rioske <rioskenakamura@gmail.com>
2024-11-14 16:43:40 -0400

    s

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
 let cacheName = "metador-de-veia";
 let filesToCache = ["/", "/index.html", 
-                "/css/style.css", "/js/main.js, /html/carne, /html/doces, /html/massas, /images/, favicon.ico",
+                "/css/style.css", "/js/main.js", "/html/carne", "/html/doces", "/html/massas", "/images/", "favicon.ico",
                 "/images/carne1.jpg", "/images/carne2.jpg", "/images/carne3.jpg", "/images/carne4.jpg", "/images/carne5.jpg",
                 "/images/doce1.jpg", "/images/doce2.jpg", "/images/doce3.jpg", "/images/doce4.jpg", "/images/doce5.jpg",
-                "/images/massa1.jpg", "/images/massa2.jpg", "/images/massa3.jpg", "/images/massa4.jpg", "/images/massa5.jpg",
-                "/images/massa6.jpg", "/images/massa7.jpg", "/images/massa8.jpg", "/images/massa9.jpg", "/images/massa10.jpg", 
+                "/images/massa1.jpg", "/images/massa2.jpg", "/images/massa3.jpg", "/images/massa4.jpg", 
+                "/images/massa6.jpg", "/images/massa8.jpg", "/images/massa9.jpg", "/images/massa10.jpg", 
+                
               ];
 
 /* inicializando a service worker e fazendo o 
